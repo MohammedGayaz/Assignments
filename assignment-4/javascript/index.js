736f936a9f9fa5d835ef9961304408b37b29ba59
MohammedGayaz <sheikgayazuddin@gmail.com>
2024-07-15 21:56:33 +0530

    added assignment 4

---
+++ assignment-4/javascript/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello world")
+})
+
+app.listen(3000, () =>  console.groupCollapsed("listening at port 3000"))
